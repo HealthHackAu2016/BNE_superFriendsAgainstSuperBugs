5cb13c0e225419297a6bec3745c5ce4ab8180256
jsokol89 <jacek.sokolowski@yahoo.com>
2016-10-15 21:24:31 +1000

    Sample model + vanilla sample template

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,5 +2,15 @@
 from django.db import models
 
 
-class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+class Sample(models.Model):
+    date = models.DateField()
+    hospital = models.CharField(max_length=15)
+    doctor = models.CharField(max_length=25)
+    gender = models.IntegerField()
+    age_group = models.IntegerField()
+    postcode = models.IntegerField()
+    country = models.CharField(max_length=3)
+    travel_last_6_m = models.CharField(max_length=3)
+    condition = models.TextField()
+    allergies_ab = models.TextField()
+    current_ab = models.TextField()
