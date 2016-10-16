a68dc620753cbda413161a5d53ad31612a5f8e58
jsokol89 <jacek.sokolowski@yahoo.com>
2016-10-16 13:46:46 +1000

    Removing countires leftovers, fixing up frontend

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_countries',
     'bootstrap_toolkit',
     'myproject.myapp'
 )
